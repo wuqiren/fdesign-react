6a8acb4ca24d52d1d671f92ce3894ab5972c7694
fishfan <fishfan@tencent.com>
2022-08-01 18:54:16 +0800

    透传方法

--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,23 +1,28 @@
-import React from 'react'
+import React,{useState,createContext} from 'react'
 import classnames from 'classnames'
 
 
 type MenuMode = 'vertical' | 'horizontal' | 'inline'
-
+type SelectBack = (selectedIndex: number) => void;
 interface MenuProps {
   className?: string;
   mode?: MenuMode;
   style?: React.CSSProperties;
   defaultIndex?: number;
   children: React.ReactNode;
-  onSelect?: (selectedIndex: number) => void;
-  
+  onSelect?: SelectBack;
 }
+interface IMenuContext {
+  index?: number;
+  onSelect?: SelectBack;
+}
+export const MenuContxt = createContext<IMenuContext>({index:0 })
 const Menu = (props: MenuProps) => {
   const { className, mode, style, defaultIndex,children } = props;
   const classes = classnames('f-menu',className, {
     
   })
+
   return <ul style={style} className={classes}>
     {children}
   </ul>
